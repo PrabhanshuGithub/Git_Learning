680b099c1b42dacfb5737c6950f860501dceda90
root <root@cloudserver-1t8gegn2.mhc>
2023-03-16 00:55:16 +0100

    to test push after fetch

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+class helloworld
+{
+public static void main(String[] args){
+	System.out.println("------Console-Output--------");
+  System.out.println("hello World");	
+	System.out.println("------Console-Output--------");
+}
+}
